ee6d2e0ec94303038b06b1d46fff1a1a236c0851
Cyril Demand <cydemand@gmail.com>
2026-08-25 21:30:49 +0200

    init: nextjs repo

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen w-full justify-center">
+      <main>
+        <h1>Le beau site de Louise</h1>
+      </main>
+    </div>
+  );
+}
