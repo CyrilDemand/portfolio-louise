fef1c7f7c81deef9d8601391ed5c132b4dd46af6
Cyril Demand <cydemand@gmail.com>
2026-08-25 22:35:34 +0200

    fix: init about

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen w-full justify-center">
+      <main>
+        <h1>page about</h1>
+      </main>
+    </div>
+  );
+}
